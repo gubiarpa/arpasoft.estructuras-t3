3e165b74d10e718825d6fa805a55553b10342f05
Billy Arredondo <billy.arredondo@pucp.pe>
2022-11-21 07:50:54 -0500

    Create Map Behavior
    - IMapService

---
+++ garredondo.evaluacion-t3.core/Services/IMapService.cs
@@ -0,0 +1,35 @@
+﻿using garredondo.evaluacion_t3.core.Behavior;
+
+namespace garredondo.evaluacion_t3.core.Services
+{
+    public interface IMapService<T> where T : IEntityWithID
+    {
+        /// <summary>
+        /// Agrega un nodo
+        /// </summary>
+        /// <param name="node">Datos del nodo</param>
+        void AddNode(T node);
+
+        /// <summary>
+        /// Agrega una conexión entre dos nodos
+        /// </summary>
+        /// <param name="id1">ID del nodo 1</param>
+        /// <param name="id2">ID del nodo 2</param>
+        void AddConnection(int id1, int id2);
+
+        /// <summary>
+        /// Devuelve true si existe la conexión entre dos nodos
+        /// </summary>
+        /// <param name="id1">ID del nodo 1</param>
+        /// <param name="id2">ID del nodo 2</param>
+        /// <returns></returns>
+        bool ConnectionExists(int id1, int id2);
+
+        /// <summary>
+        /// Devuelve la lista de nodos adyacentes
+        /// </summary>
+        /// <param name="id">ID del nodo</param>
+        /// <returns>Devuelve la lista de nodos adyacentes</returns>
+        IEnumerable<T>? GetAdjacentNodesByID(int id);
+    }
+}
